777fd7033e1665a6ac1f59f9117b3b97f29894ab
rojarfranklin <59139492+rojarfranklin@users.noreply.github.com>
2020-07-22 00:39:43 +0530

    Add files via upload

---
+++ week1/maxarray.js
@@ -0,0 +1,14 @@
+
+var arr = ([1,2,3,4,5,6]) ;
+
+var max = arr(0);
+for(var i=0;i<arr.length;i++) {
+     if (arr[i]>=max) {
+     max=arr[i];
+
+        }
+
+    }
+console.log(max);
+
+
---
+++ week1/sumarray.js
@@ -0,0 +1,12 @@
+
+sum([1,2,3,4,5,6])
+function sum(arr){
+    var s=0;
+    for(var i=0;i<arr.length;i++){
+        s+=arr(i);
+
+    }
+    console.log(s);
+
+
+}
